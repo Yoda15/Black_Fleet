e7f6ec030c38711b2b143771a2a76ac715cc4c8e
gryfentitane <102817656+gryfentitane@users.noreply.github.com>
2022-04-26 17:44:45 +0200

    Plateau

---
+++ Dev/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "objet.h"
+
+int main()
+{
+    S_Case P[Larg][Long];
+    int i=0,j=0;
+    initialiser_Plateau(P);
+     for (i=0;i<Larg;i++)
+    {
+        for (j=0;j<Long;j++)
+        {
+            printf(" %d ",P[i][j].type);
+        }
+        printf("\n");
+    }
+    return 0;
+}
