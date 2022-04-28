12c882850acc68f4f186d2b9ee71879a49017549
Yoda15 <58658642+Yoda15@users.noreply.github.com>
2022-04-28 23:35:20 +0200

    Rangement
    Division_Dev

--- Dev/Affichage.h
+++
@@ -1,9 +0,0 @@
-#ifndef AFFICHAGE_H_INCLUDED
-#define AFFICHAGE_H_INCLUDED
-#include "objet.h"
-
-void Affichage_Plateau(S_Case P[][Long]);
-void color(int couleurDuTexte,int couleurDeFond);
-
-
-#endif // AFFICHAGE_H_INCLUDED
---
+++ Dev/Dev_Affichage_Plateau/Affichage.h
@@ -0,0 +1,9 @@
+#ifndef AFFICHAGE_H_INCLUDED
+#define AFFICHAGE_H_INCLUDED
+#include "objet.h"
+
+void Affichage_Plateau(S_Case P[][Long]);
+void color(int couleurDuTexte,int couleurDeFond);
+
+
+#endif // AFFICHAGE_H_INCLUDED
