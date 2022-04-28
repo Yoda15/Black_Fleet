ac35def94b47ff6efce3b8a17c71836b6cb504e9
Yoda15 <58658642+Yoda15@users.noreply.github.com>
2022-04-28 23:41:23 +0200

    Jonction_Joueurs_Objets
    Mise en Commun de tout les types objets.

--- Black_Fleet_Odeau_Sereno/Objets.h
+++ Black_Fleet_Odeau_Sereno/Objets.h
@@ -23,4 +23,20 @@ struct Carte{
 
 typedef struct Carte Carte;
 
+//Définition de l'objet "Joueur"
+struct Joueur{
+    char nom[NBCHAR]; //Nom du joueur
+    int couleur; //Couleur (choix parmis les 16)
+    int nb; //Numéro de passage (0,1,2,3)
+    int doublons; //nombre de doublons
+    //struct Carte Pioche; //Carte détenu par le joueur
+    //struct Bateau Boat; //Bateau détenu par le joueur
+};
+typedef struct Joueur Joueur;
+/*
+//Définition de l'objet "Bateau"
+struct Bateau{
+};
+*/
+
 #endif // OBJETS_H_INCLUDED
